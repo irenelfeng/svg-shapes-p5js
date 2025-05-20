3a556458b9d9d852781ef0a19a426f514e27bae1
Ms. Feng <irene_feng@horacemann.org>
2025-05-20 12:44:43 -0400

    add contains() and getVertex() and getVertexCount()

--- README.md
+++ README.md
@@ -3,13 +3,16 @@ A p5.js library for loading and creating SVGs.
 
 ## Features
 
-+ loadSVG() - loads an SVG file and converts it to a p5.js shape
-+ disableStyle() - disables (and enables) the style of the SVG 
-+ MIT License
++ `loadSVG()` - loads an SVG file and converts it to a p5.js shape
++ Features of SVGShape
++ `disableStyle()/enableStyle()` - disables (and enables) the style of the SVG 
++ `contains()` method to check if a point is inside the shape
++ `getVertexCount()` and `getVertex(i)` methods to get the number of vertices and the vertex at a given index for the path variable. 
 
 ## Installation
 Link ShapesSVG.js in your HTML file on p5.js:
-```
+
+```javascript
 <head>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>
     <script src="ShapeSVG.js"></script>
@@ -18,8 +21,16 @@ Link ShapesSVG.js in your HTML file on p5.js:
 ```
 
 ## Usage
-[Processing example]https://editor.p5js.org/ifenghm/sketches/yHnx6FDvb)
+[Processing example](https://editor.p5js.org/ifenghm/sketches/yHnx6FDvb)
+
+## References 
+
+[Processing java code]https://github.com/processing/processing4/blob/main/core/src/processing/core/PShapeSVG.java#L1040
+[SVG specification](https://www.w3.org/TR/SVG2/)
 
 ## TODO
-- [ ] Add getVertexCount() and getVertex() methods to get the number of vertices and the vertex at a given index for the path variable. 
+- [ ] Contribute to the [libraries](https://github.com/processing/p5.js-website/blob/main/docs/contributing_libraries.md) p5.js website
 - [ ] Add more primitives (only rectangle, circle, and ellipse are implemented)
+- [ ] Add more shape geometry methods (e.g., `getBounds()`, `getCenter()`, etc.)
+
+## MIT License
--- ShapeSVG.js
+++ ShapeSVG.js
@@ -1,7 +1,10 @@
 // Converted from processing to p5.js
+// Copyright (c) 2025 irenelfeng
+// https://github.com/processing/processing4/blob/main/core/src/processing/core/PShapeSVG.java
 // https://www.perplexity.ai/search/can-you-rewrite-this-pshapesvg-rIK.abgqRmeZqof1.DLK7A
-// TODO: consider await / async without using .then syntax
+
 p5.prototype.loadSVG = async function (path) {
+  // TODO: consider await / async without using .then syntax
   let thing = fetch(path)
     .then((response) => {
       if (!response.ok) {
@@ -30,6 +33,12 @@ class ShapeSVG {
     this.type = null;
     this.svgElement = svgElement;
     this.style = true;
+    this.vertices = [];
+    this.vertexCodes = [];
+    this.close = false;
+    // vertexCodes can have a different length than vertices (less)
+    // ex: M0,0 C1,1 2,2 3,3 
+    // has a length of 4 vertices but 2 vertex codes (M and C)
 
     // Parse attributes
     this._parseAttributes(svgElement);
@@ -70,6 +79,8 @@ class ShapeSVG {
     switch (element.tagName.toLowerCase()) {
       case 'path':
         this.pathData = element.getAttribute('d');
+        // adds vertices 
+        this._parsePath(this.pathData);
         this.type = 'path';
         break;
       case 'rect':
@@ -86,6 +97,7 @@ class ShapeSVG {
         break;
       case 'g':
         this.type = 'group';
+        break;
     }
   }
 
@@ -99,6 +111,243 @@ class ShapeSVG {
     }
     return matrix;
   }
+
+  /**
+   * helper function that loads the path buffer and adds some | to it 
+   * @param {string} pathData
+   * @returns {string} pathBuffer 
+   */
+  _loadPathBuffer(pathData) {
+    let pathBuffer = '';
+    if (!pathData || pathData.trim().length === 0) return;
+    const pathDataChars = pathData.split('');
+
+    let LexState = {
+      AFTER_CMD: 0,
+      NEUTRAL: 1,
+      INTEGER: 2,
+      DECIMAL: 3, // 
+      EXP_HEAD: 4, // svgs denote exponents with 'e'
+      EXP_TAIL: 5 //
+    };
+    let lexState = LexState.NEUTRAL;
+
+    for (const element of pathDataChars) {
+      let c = element;
+
+      if (lexState === LexState.AFTER_CMD) {
+        pathBuffer += '|';
+        lexState = LexState.NEUTRAL;
+      }
+
+      if (c >= '0' && c <= '9') {
+        if (lexState === LexState.NEUTRAL) {
+          lexState = LexState.INTEGER;
+        } else if (lexState === LexState.EXP_HEAD) {
+          lexState = LexState.EXP_TAIL;
+        }
+        pathBuffer += c;
+        continue;
+      } else if (c === '-') {
+        if (lexState === LexState.NEUTRAL) {
+          lexState = LexState.INTEGER;
+        } else if (lexState === LexState.EXP_HEAD) {
+          lexState = LexState.EXP_TAIL;
+        } else {
+          pathBuffer += '|';
+          lexState = LexState.INTEGER;
+        }
+        pathBuffer += '-';
+        continue;
+      } else if (c === '.') {
+        if (lexState === LexState.DECIMAL || lexState === LexState.EXP_HEAD || lexState === LexState.EXP_TAIL) {
+          pathBuffer += '|';
+        }
+        pathBuffer += '.';
+        lexState = LexState.DECIMAL;
+        continue;
+      } else if (c === 'e' || c === 'E') {
+        pathBuffer += 'e';
+        lexState = LexState.EXP_HEAD;
+        continue;
+      }
+
+      if (lexState !== LexState.NEUTRAL) {
+        pathBuffer += '|';
+        lexState = LexState.NEUTRAL;
+      }
+
+      if (c !== ',') {
+        pathBuffer += c;
+      }
+
+      if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+        lexState = LexState.AFTER_CMD;
+      }
+    }
+    return pathBuffer;
+  }
+
+  // TODO: fix this, it's not working for remembering curve vertices 
+  _parsePath(pathData) {
+    // Tokenize by | and whitespace
+    const pathBuffer = this._loadPathBuffer(pathData);
+    const pathTokens = pathBuffer.split(/[\|\s]+/).filter(Boolean);
+
+    // Now process tokens as in the Java code
+    let cx = 0, cy = 0, movetoX = 0, movetoY = 0;
+
+    let i = 0, implicitCommand = null, prevCurve = false;
+    let ctrlX = 0, ctrlY = 0; // idk why i'm not using these, debug later
+
+
+    while (i < pathTokens.length) {
+      let c = pathTokens[i][0];
+
+      if ((c >= '0' && c <= '9') || c === '-' || c === '.') {
+        c = implicitCommand;
+        i--;
+      } else {
+        implicitCommand = c;
+      }
+      switch (c) {
+
+        case 'M':
+          cx = parseFloat(pathTokens[i + 1]);
+          cy = parseFloat(pathTokens[i + 2]);
+          movetoX = cx;
+          movetoY = cy;
+          this.vertices.push([cx, cy]);
+          this.vertexCodes.push('M');
+          implicitCommand = 'L';
+          i += 3;
+          break;
+        case 'm': // relative move
+          cx += parseFloat(pathTokens[i + 1]);
+          cy += parseFloat(pathTokens[i + 2]);
+          movetoX = cx;
+          movetoY = cy;
+          this.vertices.push([cx, cy]);
+          this.vertexCodes.push('m');
+          implicitCommand = 'l';
+          i += 3;
+          break;
+        case 'L':
+          cx = parseFloat(pathTokens[i + 1]);
+          cy = parseFloat(pathTokens[i + 2]);
+          this.vertices.push([cx, cy]);
+          this.vertexCodes.push('L');
+          i += 3;
+          break;
+        case 'l':
+          cx += parseFloat(pathTokens[i + 1]);
+          cy += parseFloat(pathTokens[i + 2]);
+          this.vertices.push([cx, cy]);
+          this.vertexCodes.push('l');
+          i += 3;
+          break;
+        case 'C': {
+          // Cubic Bezier curve
+          const x1 = parseFloat(pathTokens[i + 1]);
+          const y1 = parseFloat(pathTokens[i + 2]);
+          const x2 = parseFloat(pathTokens[i + 3]);
+          const y2 = parseFloat(pathTokens[i + 4]);
+          cx = parseFloat(pathTokens[i + 5]);
+          cy = parseFloat(pathTokens[i + 6]);
+          this.vertices.push([x1, y1]);
+          this.vertices.push([x2, y2]);
+          this.vertices.push([cx, cy]);
+          this.vertexCodes.push('C');
+          i += 7;
+          break;
+        }
+        case 'c': {
+          // Cubic Bezier curve (relative)
+          const x1c = cx + parseFloat(pathTokens[i + 1]);
+          const y1c = cy + parseFloat(pathTokens[i + 2]);
+          const x2c = cx + parseFloat(pathTokens[i + 3]);
+          const y2c = cy + parseFloat(pathTokens[i + 4]);
+          cx += parseFloat(pathTokens[i + 5]);
+          cy += parseFloat(pathTokens[i + 6]);
+          this.vertices.push([x1c, y1c]);
+          this.vertices.push([x2c, y2c]);
+          this.vertices.push([cx, cy]);
+          this.vertexCodes.push('c');
+          i += 7;
+          break;
+        }
+        // TODO (repeat for other commands: H, h, V, v, C, c, S, s, Q, q, T, t, A, a, Z, z)
+        //       case 'H': // Horizontal line
+        //         const x = type === 'h' ? currentX + args[0] : args[0];
+        //         this.vertices.push({ x, y: currentY });
+        //         currentX = x;
+        //         break;
+
+        //       case 'V': // Vertical line
+        //         const y = type === 'v' ? currentY + args[0] : args[0];
+        //         this.vertices.push({ x: currentX, y });
+        //         currentY = y;
+        //         break;
+        case 'Z':
+        case 'z':
+          cx = movetoX;
+          cy = movetoY;
+
+          this.vertexCodes.push('Z');
+          i++;
+          close = true;
+          break;
+        default:
+          throw new Error('shape command not handled: ' + pathTokens[i]);
+      }
+      // prevCommand = c;
+    }
+
+    // return { vertices, vertexCodes };
+  }
+
+
+  // _parsePathData(d) {
+  //   const commands = d.match(/[A-Za-z][^A-Za-z]*/g) || [];
+  //   let currentX = 0;
+  //   let currentY = 0;
+
+  //   commands.forEach(cmd => {
+  //     const type = cmd[0];
+  //     const args = cmd.slice(1).trim().split(/[\s,]+/).map(parseFloat);
+
+  //     switch (type.toUpperCase()) {
+  //       case 'M': // MoveTo
+  //       case 'L': // LineTo
+  //         for (let i = 0; i < args.length; i += 2) {
+  //           const x = type === 'm' ? currentX + args[i] : args[i];
+  //           const y = type === 'm' ? currentY + args[i + 1] : args[i + 1];
+  //           this.vertices.push({ x, y });
+  //           currentX = x;
+  //           currentY = y;
+  //         }
+  //         break;
+
+  //       case 'H': // Horizontal line
+  //         const x = type === 'h' ? currentX + args[0] : args[0];
+  //         this.vertices.push({ x, y: currentY });
+  //         currentX = x;
+  //         break;
+
+  //       case 'V': // Vertical line
+  //         const y = type === 'v' ? currentY + args[0] : args[0];
+  //         this.vertices.push({ x: currentX, y });
+  //         currentY = y;
+  //         break;
+
+  //       case 'Z': // Close path
+  //         if (this.vertices.length > 0) {
+  //           this.vertices.push({ ...this.vertices[0] });
+  //         }
+  //         break;
+  //     }
+  //   });
+  // }
 
   _createRectPath(element) {
     const x = parseFloat(element.getAttribute('x')) || 0;
@@ -132,7 +381,7 @@ class ShapeSVG {
           ctx.fillStyle = this.styles.fill;
           ctx.fill(new Path2D(this.pathData));
         }
-  
+
         if (this.styles.stroke !== "none") {
           ctx.strokeStyle = this.styles.stroke;
           ctx.lineWidth = this.styles.strokeWidth;
@@ -157,11 +406,37 @@ class ShapeSVG {
 
   /**
    * Gets child by searching by name (svg id attribute)
-   * @param {string} name 
+   * @param {number} index
    * @returns {ShapeSVG} child
    */
-  getChild(name) {
-    return this.children.find(child => child.name === name);
+  getChild(index) {
+    return this.children[index];
+  }
+
+  /**
+   * @returns {number} number of SVG children
+   */
+  getChildCount() {
+    return this.children.length;
+  }
+
+  /**
+    * @returns {number} number of vertices
+    */
+  getVertexCount() {
+    return this.vertices.length;
+  }
+
+  getVertexCode(index) {
+    this.getVertex(index);
+    return this.vertexCodes[index];
+  }
+
+  getVertex(index) {
+    if (index < 0 || index >= this.getVertexCount()) {
+      throw new Error(`Vertex index ${index} out of bounds for vertex length ${this.getVertexCount()}`);
+    }
+    return this.vertices[index];
   }
 
   /**
@@ -192,6 +467,71 @@ class ShapeSVG {
 
     for (const element of this.children) {
       element.enableStyle();
+    }
+  }
+  /**
+   * Helper function get invert 
+   * @param {p5.Vector} p , has an x and y property
+   * @returns 
+   */
+  inverseMatrix(p) {
+    if (this.matrix) {
+      const inverseCoords = this.matrix.get();
+      inverseCoords.invert();  // Double invert from original Java code
+      inverseCoords.invert();
+      return inverseCoords.mult(p);
+    }
+  }
+
+  /**
+   * 
+   * @param {number} x-coord
+   * @param {number} y-coord
+   * @returns {boolean} true if the point is inside this shape
+   */
+  contains(x, y) {
+    // just monkey-transcribed from PShape java code. dunno what it does
+    // https://github.com/processing/processing4/blob/main/core/src/processing/core/PShape.java#L3104
+    if (this.getType() === 'path' || this.getType() === 'geometry') {
+      let p = { x: x, y: y }; // p is a ray from (0,0) to (x,y)
+
+      // Apply inverse transformation if necessary
+      this.inverseMatrix(p);
+
+      let inside = false;
+
+      // Check if the point is inside any triangle formed by the pairwise vertices and the last point 
+      // in a vertex of n points, then n-1 
+
+
+      // https://wrfranklin.org/Research/Short_Notes/pnpoly.html
+      for (let i = 0, j = this.getVertexCount() - 1; i < this.getVertexCount(); j = i++) {
+        const xi = this.vertices[i][0];
+        const yi = this.vertices[i][1];
+
+        const xj = this.vertices[j][0];
+        const yj = this.vertices[j][1];
+        //console.log(`x_i, y_i ${xi}, ${yi}, j: (${xj}, ${yj}`);
+
+        const yMatch = (yi > p.y) !== (yj > p.y); // cross y test 
+        const xIntersect = (xj - xi) * (p.y - yi) / (yj - yi) + xi;
+
+        if (yMatch && p.x < xIntersect) {
+          inside = !inside;
+        }
+      }
+      return inside;
+
+    } else if (this.getType() === 'group') {
+      for (let i = 0; i < this.getChildCount(); i++) {
+        if (this.children[i]?.contains(x, y)) {
+          return true;
+        }
+      }
+      return false;
+
+    } else {
+      throw new Error('contains() only supported for paths and groups');
     }
   }
 }
